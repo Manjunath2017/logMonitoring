567c84c55eb47903cfc6787a7e9e6292b8f0d3c6
Manjunath2017 <manjunath.jadhav1231@gmail.com>
2020-02-06 12:10:38 +0530

    open a file and emit data

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const server=http.createServer(app);
 const socket=require('socket.io');
 const io=socket(server);
 
+const exec = require('child_process').exec;
+const fs=require('fs');
+const fileName = "logger.txt";
+
+
 require('./writelogs').writeLogs();
 
 app.get('/', (request, response)=>{
@@ -14,6 +19,37 @@ app.get('/', (request, response)=>{
 
 io.on('connection', (socket)=>{
     console.log('socket connected!');
+    fs.open(fileName, 'r', (err)=> { 
+        // console.log(socket);
+
+        exec("tail " + fileName, (err, tailLogs) => { //tailLogs is the content of file 
+        //this will execute only once(1st time)
+            tailLogs = tailLogs.replace(new RegExp('\n','g'), '<br />');  //replace --> \n to <br /> --> 'g' global replace all matches cases at once
+            if(!tailLogs){
+                tailLogs = "<h3>Log File Empty</h3>"; //display if file is empty
+            }
+        // emit throws the message to client or browser
+            socket.emit('logsSync', { message: tailLogs });
+        });
+
+        //after every update in file 'watch' will triger
+        fs.watch(fileName, (event)=>{
+// console.log(event);
+            if(event === "change") { 
+                //google execute shell script
+                exec("tail " + fileName, (err, tailLogs) => {
+                    tailLogs = tailLogs.replace(new RegExp('\n','g'), '<br />'); //replace --> \n to <br /> --> 'g' global replace all matches cases at once
+                    if(!tailLogs){
+                        tailLogs = "<h3>Log File Empty</h3>";
+                    }
+                socket.emit('logsSync', { message: tailLogs });
+                });
+            }
+        });
+    });
+
+
+
 });
 
 const port=process.env.PORT || 8080;
